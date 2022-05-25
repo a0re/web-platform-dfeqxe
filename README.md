0910b550e80c297c38d93713114285ddf10025bf
a0re <jvoong1517@gmail.com>
2022-05-25 14:17:54 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-dfeqxe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-dfeqxe)
---
+++ script.js
@@ -0,0 +1,13 @@
+let cookiecounter = 0;
+let generator = 0;
+let clicks = 0;
+
+function clickbutton() {
+  document.getElementById(cookies).innerHTML;
+  clicks++;
+  cookiecounter++;
+}
+
+if (clicks < 10) {
+  generator++;
+}
